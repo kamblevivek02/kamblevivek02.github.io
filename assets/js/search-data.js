2e2ea059e41ced2bdc9a121802c6ea326359d140
kamblevivek02 <kamblevivek02@gmail.com>
2026-04-12 15:39:53 +0000

    Deploying to gh-pages from @ kamblevivek02/kamblevivek02.github.io@cffe3785288578bd2d29e37187a81fb426ba4db9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,13 +44,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/contact/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-vipassana",
           title: "Vipassana",
           description: "",

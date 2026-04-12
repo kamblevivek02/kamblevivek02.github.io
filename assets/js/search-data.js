db72d5d111e80548dd896dd93c376c9b6f93b8c1
kamblevivek02 <kamblevivek02@gmail.com>
2026-04-12 15:39:08 +0000

    Deploying to gh-pages from @ kamblevivek02/kamblevivek02.github.io@1ab3eb3adec4be6f5285c9959feb35c6471870eb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,13 +65,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "nav-vipassana",
           title: "Vipassana",
           description: "",

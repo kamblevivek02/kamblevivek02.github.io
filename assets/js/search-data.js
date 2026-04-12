28d3ed7c3b3f513ef131ea80ce6e5201e98d8df3
kamblevivek02 <kamblevivek02@gmail.com>
2026-04-12 15:39:15 +0000

    Deploying to gh-pages from @ kamblevivek02/kamblevivek02.github.io@0f3dbf89d7271c088f90b82b5ebef6dcea5b8e18 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,13 +44,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/achievements/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-contact",
           title: "Contact",
           description: "",
